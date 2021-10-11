95a093f5b13c15b2869d4570476dfbebb0497056
ataberkdw <ataberkturkoglug3@gmail.com>
2021-10-12 02:46:34 +0300

    clean-blog-proje-odev-9

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const ejs = require('ejs');
+const Post = require('./models/Post');
+const mongoose = require('mongoose');
 
 const app = express();
+//connect db
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 //Template Engine
 app.set("view engine", "ejs");
 //Middlewares
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 
-
-const blog = {
-    id:1,
-    titel:'Blog title',
-    description:'Blog description'
-}
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get('/', async (req,res)=>{
+    const posts = await Post.find({});
+    res.render('index',{
+        posts
+    })
 })
 app.get('/about',(req,res)=>{
     res.render('about')
@@ -25,5 +28,10 @@ app.get('/add_post',(req,res)=>{
 app.get('/post',(req,res)=>{
     res.render('post')
 })
+app.post('/posting', async (req,res)=>{
+    await Post.create(req.body)
+    res.redirect('/')
+})
+
  
 app.listen(3000)
