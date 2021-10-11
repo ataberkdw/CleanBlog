d5600adaef1a34e740d2a1d5531254acf1caa88b
ataberkdw <ataberkturkoglug3@gmail.com>
2021-10-11 22:34:34 +0300

    patika-nodejs-odev-8

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+//Template Engine
+app.set("view engine", "ejs");
+//Middlewares
+app.use(express.static('public'))
+
+
 const blog = {
     id:1,
     titel:'Blog title',
     description:'Blog description'
 }
 app.get('/',(req,res)=>{
-    res.send(blog);
+    res.render('index')
 })
-
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+app.get('/add_post',(req,res)=>{
+    res.render('add_post')
+})
+app.get('/post',(req,res)=>{
+    res.render('post')
+})
+ 
 app.listen(3000)
